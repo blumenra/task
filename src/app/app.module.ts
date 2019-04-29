ad26b2830bf1ea7a46ee8acf2c4bcb85f077c13d
Alon <alon@mail.com>
2019-04-29 22:42:38 +0300

    Removed component background because I found it unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './background/background.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
