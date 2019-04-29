913c0f077e64bc363cd38fea1bf7c13b12de8624
Alon <alon@mail.com>
2019-04-30 00:49:11 +0300

    Created a new component for the possible answers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AnswersComponent } from './answers/answers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnswersComponent
   ],
   imports: [
     BrowserModule
