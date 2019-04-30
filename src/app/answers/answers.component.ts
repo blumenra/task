9bb56e50ff3be2e9a8577d5173ea5d36f50b47cf
Alon <alon@mail.com>
2019-05-01 00:12:43 +0300

    Finished adding hover sign aside to an answer

--- src/app/answers/answers.component.ts
+++ src/app/answers/answers.component.ts
@@ -7,16 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnswersComponent implements OnInit {
 
+  options:boolean[] = [];
+  currHoveredOption:number;
   answers:string[] = [];
+  isMouseOver:boolean = false;
+
   constructor() { 
+
     this.answers.push("Demeter", "Zeus", "Athena", "Apollo");
-    
+    this.initOptions();
+    this.currHoveredOption = -1;
 
     this.shuffle();
   }
 
+  initOptions(){
+    for(let i=0; i < this.answers.length; i++)
+      this.options.push(false);
+  }
+  
+  onMouseOver(event){
+    this.currHoveredOption = event.srcElement.id;
+    this.options[this.currHoveredOption] = !this.options[this.currHoveredOption];
+  }
+
+  onMouseOut(event){
+    
+    this.options[this.currHoveredOption] = !this.options[this.currHoveredOption];
+    this.currHoveredOption = -1;
+  }
+
+  flipMouseOverVal(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
   shuffle(){
-      let j, x, i;
+      let j:number, x:string, i:number;
       for (i = this.answers.length - 1; i > 0; i--) {
           j = Math.floor(Math.random() * (i + 1));
           x = this.answers[i];
