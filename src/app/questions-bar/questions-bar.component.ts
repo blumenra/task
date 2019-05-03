332084a2e9c54d02278ae928ccd9cf85737a28a7
Alon <alon@mail.com>
2019-05-03 17:52:42 +0300

    Finished implementing the questions bar track design

--- src/app/questions-bar/questions-bar.component.ts
+++ src/app/questions-bar/questions-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { ChangeDetectorStatus } from '@angular/core/src/change_detection/constants';
+import { Circle } from '../Models/Circle';
 
 @Component({
   selector: 'app-questions-bar',
@@ -11,18 +11,12 @@ export class QuestionsBarComponent implements OnInit, OnChanges {
   @Input() public numOfQuestions:number;
   @Input() public answeredCount:number;
 
-  circles:any[];
-  count:number;
-  classes = {};
+  circles:Circle[];
   initialized:boolean;
 
   constructor() {
     this.initialized = false;
     this.numOfQuestions = -1;
-    this.classes = {
-      wrapper: false,
-      trunOn: true
-    }
   }
 
   ngOnChanges(chages:SimpleChanges){
@@ -31,11 +25,18 @@ export class QuestionsBarComponent implements OnInit, OnChanges {
       this.initBar();
       this.initialized = true;
     }
+
+    if(this.initialized && chages.answeredCount){
+      this.circles[this.answeredCount].classes['turnOn'] = true;
+    }
   }
     
   initBar(){
     
-    this.circles = Array(this.numOfQuestions);
+    this.circles = Array<Circle>(this.numOfQuestions);
+    for(let i=0; i < this.circles.length; i++){
+      this.circles[i] = { classes: {default: true, trunOn: false}};
+    }
   }
 
   ngOnInit() {
